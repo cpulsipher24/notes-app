f5b48962a14796b2c0958fa1962844c70f515170
Cpulsipher24 <collintpulsipher@gmail.com>
2024-01-29 19:04:29 -0700

    Fixed pathing issues

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,12 +3,12 @@ const router = require('express').Router();
 
 // Landing page
 router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+  res.sendFile(path.join(__dirname, '../Develop/public/index.html'));
 });
 
 // Notes page
 router.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/notes.html'));
+    res.sendFile(path.join(__dirname, '../Develop/public/notes.html'));
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-// server.js
-
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -12,9 +12,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Serve static files (CSS, client-side JS, etc.)
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'Develop', 'public')));
 
-// Your routes will go here...
+// Use HTML routes
+app.use('/', htmlRoutes);
+
+// Use API routes
+app.use('/', apiRoutes);
 
 // Start the server
 app.listen(PORT, () => {
